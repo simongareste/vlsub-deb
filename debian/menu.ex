6352328a3f0a29ccc7661337ba5b8a35dacb75f7
Simon GARESTE <simon.gareste@scilab-enterprises.com>
2013-09-17 11:09:30 +0200

    removing examples, updating various files

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(vlc-plugin-vlsub):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="vlc-plugin-vlsub" command="/usr/bin/vlc-plugin-vlsub"
--- debian/vlc-plugin-vlsub.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the vlc-plugin-vlsub package
-#
-0 4	* * *	root	[ -x /usr/bin/vlc-plugin-vlsub_maintenance ] && /usr/bin/vlc-plugin-vlsub_maintenance
